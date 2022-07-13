a2ee7c82c15ca8252d2a7902292210fbf2f05d54
Max Rodriguez <max@max-rodriguez.com>
2022-07-13 13:32:37 -0700

    [scripts] Read argument for 'Debug' or 'Release' build type

--- configure.sh
+++ configure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Creates and configures build directory
+
+# (Script used with IDE script configurations)
+# (Set script options to either "Debug" or "Release")
+
 mkdir build
 cd build || exit
 conan install .. --build=missing
-cmake .. -G Ninja -D CMAKE_BUILD_TYPE=Debug
+cmake .. -G Ninja -D CMAKE_BUILD_TYPE="$1"
