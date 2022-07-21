359575edfdaa7381e0dfc1d1c67acd38e6e81d4f
Max Rodriguez <max@max-rodriguez.com>
2022-07-20 23:27:09 -0700

    license: Updated source code files license citations to BSD 3-clause

--- src/vulkan/SwapChain.cxx
+++ src/vulkan/SwapChain.cxx
@@ -2,19 +2,12 @@
  * SwapChain.cxx
  * Creates the Vulkan swap chain instance and swap chain buffers.
  *
- * Copyright 2022 Max Rodriguez
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ * VULKRAY ENGINE SOFTWARE
+ * Copyright (c) 2022, Max Rodriguez. All rights reserved.
+ *
+ * All use of this software is subject to the terms of the revised BSD
+ * license. You should have received a copy of this license along
+ * with this source code in a file named "COPYING."
  */
 
 #include "Vulkan.hxx"
